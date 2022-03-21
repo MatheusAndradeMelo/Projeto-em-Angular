4039e044345a31b29e1e24428c7e78187f022ee3
Matheus Andrade <93740980+MatheusAndradeMelo@users.noreply.github.com>
2022-03-20 21:07:47 -0300

    criando método criar produto conexao com api

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from "./../product.service";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Product } from "../product.model";
 
 @Component({
   selector: "app-product-create",
@@ -8,13 +9,23 @@ import { Router } from "@angular/router";
   styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(private productService: ProductService, private router: Router) {
+
+  }
+
+  product: Product = {
+    name: '',
+    price: null 
+  }
 
   ngOnInit(): void {}
 
   createProduct(): void {
     {
-      this.productService.showMessage("Produto criado!");
+      this.productService.create(this.product).subscribe(() => {
+        this.productService.showMessage("Produto criado!");
+        this.router.navigate(['/products'])
+      })
     }
   }
 
